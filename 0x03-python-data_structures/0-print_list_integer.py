0e18998d19682d74365d08cc87778aba30d92e1d
njdam <jeandamngaboyimana@gmail.com>
2023-06-12 19:45:03 +0000

    restored

--- 0x03-python-data_structures/0-print_list_integer.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-# A program to print integers in a list one in each linei
-
-
-def print_list_integer(my_list=[]):
-    lens = len(my_list)
-    for i in range(lens):
-        print("{:d}".format(my_list[i]))
